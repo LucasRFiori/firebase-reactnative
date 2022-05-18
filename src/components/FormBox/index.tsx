ac27d89787b72799cd58cc31525291fff806b103
Lucas  FIori <lucas.fiori@corebiz.ag>
2022-05-18 00:59:27 -0300

    need auth

--- src/components/FormBox/index.tsx
+++ src/components/FormBox/index.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import firestore from "@react-native-firebase/firestore"
+import { Alert } from 'react-native';
 import { Container } from './styles';
 import { ButtonIcon } from '../ButtonIcon';
 import { Input } from '../Input';
 
 export function FormBox() {
+  const [description, setDescription] = useState('')
+  const [quantity, setQuantity] = useState(0)
+
+  async function handleProductAdd() {
+    // firestore()
+    // .collection('products')
+    // .doc('my-custom-id')
+    // .set({
+    //   description,
+    //   quantity,
+    //   done: false,
+    // })
+    // .then(() => {
+    //   Alert.alert('Produto adicionado com sucesso!')
+    // }).catch((error) => console.log(error)) //add document with custom doctitle
+
+    firestore()
+    .collection('products')
+    .add({
+      description,
+      quantity,
+      done: false,
+      createdAt: firestore.FieldValue.serverTimestamp()
+    })
+    .then(() => {
+      Alert.alert('Produto adicionado com sucesso!')
+    }).catch((error) => console.log(error)) //add document
+  }
+
   return (
     <Container>
       <Input
         placeholder="Nome do produto"
         size="medium"
+        onChangeText={setDescription}
       />
 
       <Input
@@ -17,12 +48,13 @@ export function FormBox() {
         keyboardType="numeric"
         size="small"
         style={{ marginHorizontal: 8 }}
+        onChangeText={value => setQuantity(Number(value))}
       />
 
       <ButtonIcon
         size='large'
         icon="add-shopping-cart"
-        onPress={() => { }}
+        onPress={handleProductAdd}
       />
     </Container>
   );
--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FlatList } from 'react-native';
+import firestore from '@react-native-firebase/firestore'
 
 import { styles } from './styles';
 import { Product, ProductProps } from '../Product';
@@ -7,7 +8,121 @@ import { Product, ProductProps } from '../Product';
 import { shoppingListExample } from '../../utils/shopping.list.data';
 
 export function ShoppingList() {
-  const [products, setProducts] = useState<ProductProps[]>(shoppingListExample);
+  const [products, setProducts] = useState<ProductProps[]>([]);
+  
+  // useEffect(() => {
+  //   const subscribe = firestore()
+  //   .collection('products')
+  //   .orderBy('quantity')
+  //   .startAt(3)
+  //   .endAt(4) // quantidade entre 3 e 4
+  //   .onSnapshot(querySnapshot => {
+  //     const data = querySnapshot.docs.map((doc) => {
+  //       return {
+  //         id: doc.id,
+  //         ...doc.data()
+  //       }
+  //     }) as ProductProps[]
+  //     setProducts(data)
+  //   })
+
+  //   return() => subscribe();
+  // }, []) // interval docs (filtragem de precos *)
+
+  // useEffect(() => {
+  //   const subscribe = firestore()
+  //   .collection('products')
+  //   .orderBy('description', 'desc')
+  //   .onSnapshot(querySnapshot => {
+  //     const data = querySnapshot.docs.map((doc) => {
+  //       return {
+  //         id: doc.id,
+  //         ...doc.data()
+  //       }
+  //     }) as ProductProps[]
+  //     setProducts(data)
+  //   })
+
+  //   return() => subscribe();
+  // }, []) // ordering docs
+
+  // useEffect(() => {
+  //   const subscribe = firestore()
+  //   .collection('products')
+  //   .limit(3)
+  //   .onSnapshot(querySnapshot => {
+  //     const data = querySnapshot.docs.map((doc) => {
+  //       return {
+  //         id: doc.id,
+  //         ...doc.data()
+  //       }
+  //     }) as ProductProps[]
+  //     setProducts(data)
+  //   })
+
+  //   return() => subscribe();
+  // }, []) // limit search docs
+
+  // useEffect(() => {
+  //   const subscribe = firestore()
+  //   .collection('products')
+  //   .where('quantity', '==', 2)
+  //   .onSnapshot(querySnapshot => {
+  //     const data = querySnapshot.docs.map((doc) => {
+  //       return {
+  //         id: doc.id,
+  //         ...doc.data()
+  //       }
+  //     }) as ProductProps[]
+  //     setProducts(data)
+  //   })
+
+  //   return() => subscribe();
+  // }, []) // filtering docs
+
+  useEffect(() => {
+    const subscribe = firestore()
+    .collection('products')
+    .onSnapshot(querySnapshot => {
+      const data = querySnapshot.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data()
+        }
+      }) as ProductProps[]
+      setProducts(data)
+    })
+
+    return() => subscribe();
+  }, []) // search all doc in realtime
+
+  // useEffect(() => {
+  //   firestore()
+  //   .collection('products')
+  //   .get()
+  //   .then(response => {
+  //     const data = response.docs.map(doc => {
+  //       return {
+  //         id: doc.id,
+  //         ...doc.data()
+  //       }
+  //     }) as ProductProps[]
+
+  //     setProducts(data)
+  //   }).catch(error => console.log(error)) // search all documents one time
+  // }, [])
+
+  // useEffect(() => {
+  //   firestore()
+  //   .collection('products')
+  //   .doc('zVdqYSpcG6gNCWCQa7se')
+  //   .get()
+  //   .then(response => console.log({
+  //     id: response.id,
+  //     ...response.data()
+  //   }))
+  //   .catch(error => console.log(error)) //search one document
+  // })
 
   return (
     <FlatList
